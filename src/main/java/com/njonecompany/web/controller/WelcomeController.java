0471b01045d674bd26c9563865a802b1fcaad845
zkunsang <zkunsang@gmail.com>
2024-07-06 15:35:14 +0900

    Update WelcomeController.java

--- src/main/java/com/njonecompany/web/controller/WelcomeController.java
+++ src/main/java/com/njonecompany/web/controller/WelcomeController.java
@@ -15,7 +15,7 @@ public class WelcomeController {
 
     @GetMapping("/")
     public String index(Model model) {
-        logger.debug("Welcome to njonecompany.com...!");
+        logger.debug("Welcome to njonecompany.com...!!");
 
         model.addAttribute("msg", getMessage());
         model.addAttribute("today", new Date());
